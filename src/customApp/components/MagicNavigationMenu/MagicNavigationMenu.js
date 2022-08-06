9a21ff1667f55773ce8a42558e506ae2ac6f0c0d
ThanhDC <thanhdc@smartosc.com>
2022-08-06 21:27:45 +0700

    08062022

---
+++ src/customApp/components/MagicNavigationMenu/MagicNavigationMenu.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {BoxWrapper} from "../../../components/utility/box.style";
+import MenuWrapper from "./styled";
+
+const MagicNavigationMenu = props => {
+  const {} = props;
+
+  return <BoxWrapper>
+    <MenuWrapper>
+
+    </MenuWrapper>
+  </BoxWrapper>
+};
+
+export default MagicNavigationMenu;
