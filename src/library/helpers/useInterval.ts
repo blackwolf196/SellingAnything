a0b939a57ec1166eb3d1a666ec9467c4c8d7c25f
ThanhDC <blackwolf196@gmail.com>
2023-06-15 17:25:30 +0700

    Update Chart

---
+++ src/library/helpers/useInterval.ts
@@ -0,0 +1,34 @@
+import React from "react";
+
+const useInterval=(callback: () => any, delay: number | undefined) =>{
+  const intervalRef: any = React.useRef();
+  const callbackRef = React.useRef(callback);
+
+  // Remember the latest callback:
+  //
+  // Without this, if you change the callback, when setInterval ticks again, it
+  // will still call your old callback.
+  //
+  // If you add `callback` to useEffect's deps, it will work fine but the
+  // interval will be reset.
+
+  React.useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  // Set up the interval:
+
+  React.useEffect(() => {
+    if (typeof delay === 'number') {
+      intervalRef.current = window.setInterval(() => callbackRef.current(), delay);
+
+      // Clear interval if the components is unmounted or the delay changes:
+      return () => window.clearInterval(intervalRef.current);
+    }
+  }, [delay]);
+
+  // Returns a ref to the interval ID in case you want to clear it manually:
+  return intervalRef;
+};
+
+export default useInterval;
